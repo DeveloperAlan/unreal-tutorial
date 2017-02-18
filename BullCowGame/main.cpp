753238e73e5b9a7c08b9d9d41abeb8310430285e
Alan <alanng95@hotmail.com>
2017-02-18 14:05:45 +1100

    added cin, cout and getline

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	cout << "Welcome to Bulls and Cows" << endl;
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows, a fun word game" << endl;
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";	
+
+	string Guess = "";
+	getline(cin, Guess);
+	cout << Guess << endl;
 	return 0;
 }
